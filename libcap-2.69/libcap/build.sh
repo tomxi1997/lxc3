857c90d13df898b73eb519018e09f3702f1380dc
tomxi1997 <tomxi1997@gmail.com>
2025-04-03 14:22:45 +0000

    up config

--- libcap-2.69/libcap/build.sh
+++ libcap-2.69/libcap/build.sh
@@ -1,9 +1,12 @@
 # libcap specifics
-export DESTDIR=$ANDROID_LIBS
-export lib=lib
-export porefix=
+#export DESTDIR=$ANDROID_LIBS
+#export lib=lib
+#export porefix=
+make CC="$CC -Wl,-rpath=$ANDROID_LIBS/lib -Wl,--enable-new-dtags" OBJCOPY=$OBJCOPY PREFIX="$ANDROID_LIBS" PTHREADS=no PAM_CAP=no
 
-make -j4
 
-sudo make install
+
+sudo make CC="$CC -Wl,-rpath=$ANDROID_LIBS/lib -Wl,--enable-new-dtags" OBJCOPY=$OBJCOPY prefix="$ANDROID_LIBS" RAISE_SETFCAP=no lib=/lib PTHREADS=no install PAM_CAP=no
+
+#sudo make install
 
