7abae69c765e85b6c7be8ef5bbe095f6ad5e2336
tomxi1997 <tomxi1997@gmail.com>
2025-04-03 11:35:28 +0000

    1st

---
+++ lxc-3.1.0/build.sh
@@ -0,0 +1,31 @@
+export CBA_HOME=$ODMDIR
+export PREFIX=$CBA_HOME
+
+make distclean
+
+./autogen.sh
+
+CPPFLAGS="$CPPFLAGS $CFLAGS" \
+./configure --host=$BUILD_TARGET_HOST --enable-shared=yes --enable-static=yes \
+	--disable-api-docs \
+	--disable-selinux \
+	--disable-seccomp \
+	--disable-werror \
+	--enable-capabilities \
+	--disable-examples \
+	--disable-lua \
+	--disable-python \
+	--disable-bash \
+	--enable-configpath-log \
+        \
+        --disable-doc \
+        --disable-api-docs \
+	\
+	--prefix=$PREFIX \
+	--with-systemdsystemunitdir=$PREFIX/lib/systemd/system \
+	--with-config-path=$CBA_HOME/containers \
+	--with-global-conf=$CBA_HOME/.config \
+	--with-runtime-path=$PREFIX/cache
+
+make -j8
+sudo make install
